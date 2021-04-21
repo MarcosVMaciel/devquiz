d6adb9de9833a72c9454b27aa8064b37681071b6
Marcos Maciel <macielmarcos25@gmail.com>
2021-04-21 02:16:04 -0300

    aula 02 - Level Button - Quiz Card - Utilizando ListView - Criação do ChallangePage - Criação do Quiz e Awnser Widget

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key:key);
+  HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -11,7 +14,41 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBarWidget(),
+      appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Fácil"),
+                LevelButtonWidget(label: "Médio"),
+                LevelButtonWidget(label: "Difícil"),
+                LevelButtonWidget(label: "Perito")
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 8,
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget()
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
